137778f1791dad44e8f1aa2e8362c0c7983d6030
hxy-asdw <2870854049@qq.com>
2026-09-11 23:42:38 +0800

    refine 404 easter egg trail

--- script.js
+++ script.js
@@ -275,17 +275,8 @@ window.addEventListener("scroll", () => {
 }, { passive: true });
 
 if (document.body.dataset.page === "home") {
-  let clicks = 0;
-  let startedAt = 0;
-  document.querySelector(".v2-status-dot")?.addEventListener("click", () => {
-    const now = performance.now();
-    if (clicks === 0 || now - startedAt > 2000) {
-      clicks = 0;
-      startedAt = now;
-    }
-    if (++clicks === 3) {
-      window.location.assign("/the-trail-went-cold");
-    }
+  document.querySelector(".v2-trail-link")?.addEventListener("click", () => {
+    window.location.assign("/somewhere-off-the-trail/");
   });
 }
 
